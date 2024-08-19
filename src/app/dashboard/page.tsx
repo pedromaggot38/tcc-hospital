2e8597487896893ee7d409dc811dad0699a49850
Pedro Sanches <pedromaggot38@gmail.com>
2024-08-19 08:17:02 -0300

    cardItems add

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,41 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+// Exemplo de dados que poderiam ser buscados de um banco de dados.
+const cardItems = [
+  {
+    title: "Notícias",
+    description: "Quantidade total de notícias publicadas",
+    fetchCount: 123, // Esse valor poderia ser substituído por uma busca no banco de dados
+  },
+  {
+    title: "Usuários",
+    description: "Quantidade total de usuários ativos",
+    fetchCount: 45, // Esse valor poderia ser substituído por uma busca no banco de dados
+  },
+];
+
 export default function Dashboard() {
   return (
-    <main>
-      <h1 className="">Dashboard</h1>
+    <main className="sm:ml-14 p-2">
+      <section className="grid grid-cols-2 gap-2">
+        {cardItems.map((item, index) => (
+          <Card key={index}>
+            <CardHeader>
+              <div className="flex items-center justify-center">
+                <CardTitle className="sm:text-xl text-lg text-gray-800 select-none">
+                  {item.title}
+                </CardTitle>
+              </div>
+            </CardHeader>
+            <CardDescription className="text-center">
+              {item.description}
+            </CardDescription>
+            <CardContent className="text-center">
+              {item.fetchCount}
+            </CardContent>
+          </Card>
+        ))}
+      </section>
     </main>
-  )
+  );
 }
--- src/components/dashboard/sidebar/index.tsx
+++ src/components/dashboard/sidebar/index.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-import { BadgeHelp, HouseIcon, Newspaper, PanelBottom, Settings2, Users } from "lucide-react";
+import { BadgeHelp, HouseIcon, LogOut, Newspaper, PanelBottom, Settings2, Users } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -41,9 +41,11 @@ const menuItems = [
 export function Sidebar() {
     return (
         <div className="flex w-full flex-col bg-muted/40">
-            <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex">
+            <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex flex-col">
                 <nav className="flex flex-col items-center gap-4 px-2 py-5">
                     <TooltipProvider delayDuration={100}>
+                        <Image src="/logo.png" alt="Logo" width={50} height={50} className="bg-background" />
+
                         {menuItems.map((item) => (
                             <Tooltip key={item.title}>
                                 <TooltipTrigger asChild>
@@ -63,6 +65,32 @@ export function Sidebar() {
                         ))}
                     </TooltipProvider>
                 </nav>
+
+                <nav className="flex flex-col items-center gap-4 px-2 py-5 mt-auto text">
+
+                    {
+                        /*          TODO          */
+                        /*   Botão de Dark mode   */
+                    }
+
+                    <TooltipProvider delayDuration={100}>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="#"
+                                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-full hover:bg-primary/20
+                                        bg-background"
+                                >
+                                    <span className="text-lg"><LogOut className="text-red-600" /></span>
+                                    <span className="sr-only">Sair</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">
+                                Sair
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </nav>
             </aside>
 
             <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
@@ -76,17 +104,9 @@ export function Sidebar() {
                             </Button>
                         </SheetTrigger>
 
-                        <SheetContent side={"left"} className="sm:max-w-x">
+                        <SheetContent side={"left"} className="sm:max-w-x flex flex-col h-full">
                             <nav className="grid gap-6 text-lg font-medium">
-                                <Link
-                                    href={"/"}
-                                    className="flex h-10 w-10 bg-primary rounded-full text-lg items-center justify-center
-                                    text-primary-foreground md:text-base gap-2"
-                                    prefetch={false}
-                                >
-                                    <Image src="/logo.png" alt="Logo" width={50} height={50} className="bg-background" />
-                                    <span className="sr-only">Logo</span>
-                                </Link>
+                                <Image src="/logo.png" alt="Logo" width={50} height={50} className="bg-background" />
 
                                 {/* Itera sobre menuItems e cria links */}
                                 {menuItems.map((item) => (
@@ -101,7 +121,22 @@ export function Sidebar() {
                                     </Link>
                                 ))}
                             </nav>
+
+                            <nav className="mt-auto">
+
+                                {
+                                    /*          TODO          */
+                                    /*   Botão de Dark mode   */
+                                }
+                                
+                                <Link href="#" className="flex items-center space-x-4 p-2 rounded-lg hover:bg-primary/20">
+                                    <span className="text-lg"><LogOut className="text-red-600" /></span>
+                                    <span className="font-medium">Sair</span>
+                                    <span className="sr-only">Sair</span>
+                                </Link>
+                            </nav>
                         </SheetContent>
+
                     </Sheet>
 
                     <h2>Menu</h2>
