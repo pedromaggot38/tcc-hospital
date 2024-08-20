e782464a05b39e71f2777be6d4e29f41b976e356
Pedro Sanches <pedromaggot38@gmail.com>
2024-08-20 06:41:49 -0300

    Editar perfil pop-up adicionado

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,6 +1,10 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export default function Users() {
@@ -42,7 +46,88 @@ export default function Users() {
                                         <TableCell>11 99999-9999</TableCell>
                                         <TableCell>Não</TableCell>
                                         <TableCell className="p-0">
-                                            <Button>Editar</Button>
+                                            <Dialog>
+                                                <DialogTrigger asChild>
+                                                    <Button variant="outline">Edit Profile</Button>
+                                                </DialogTrigger>
+                                                <DialogContent className="sm:max-w-[500px]">
+                                                    <DialogHeader>
+                                                        <DialogTitle>Editar Perfil</DialogTitle>
+                                                        <DialogDescription>
+                                                            Faça as alterações do perfil aqui
+                                                        </DialogDescription>
+                                                    </DialogHeader>
+                                                    <div className="grid gap-4 py-4">
+                                                        <div className="grid grid-cols-4 items-center gap-4">
+                                                            <Label htmlFor="name" className="text-right">
+                                                                Nome
+                                                            </Label>
+                                                            <Input id="name" className="col-span-3" />
+                                                        </div>
+                                                        <div className="grid grid-cols-4 items-center gap-4">
+                                                            <Label htmlFor="username" className="text-right">
+                                                                Username
+                                                            </Label>
+                                                            <Input id="username" className="col-span-3" />
+                                                        </div>
+                                                        <div className="grid grid-cols-4 items-center gap-4">
+                                                            <Label htmlFor="email" className="text-right">
+                                                                E-mail
+                                                            </Label>
+                                                            <Input id="email" className="col-span-3" />
+                                                        </div>
+                                                        <div className="grid grid-cols-4 items-center gap-4">
+                                                            <Label htmlFor="phone" className="text-right">
+                                                                Telefone
+                                                            </Label>
+                                                            <Input id="phone" className="col-span-3" />
+                                                        </div>
+                                                        <div className="grid grid-cols-4 items-center gap-4">
+                                                            <Label htmlFor="image" className="text-right">
+                                                                Avatar
+                                                            </Label>
+                                                            <Input id="image" className="col-span-3" />
+                                                        </div>
+                                                        <div className="grid grid-cols-4 items-center gap-4">
+                                                            <Label htmlFor="blocked" className="text-right">
+                                                                Bloqueado
+                                                            </Label>
+                                                            <Select>
+                                                                <SelectTrigger className="w-[180px]">
+                                                                    <SelectValue placeholder="Selecione" />
+                                                                </SelectTrigger>
+                                                                <SelectContent>
+                                                                    <SelectGroup>
+                                                                        <SelectItem value="false">Não</SelectItem>
+                                                                        <SelectItem value="true">Sim</SelectItem>
+                                                                    </SelectGroup>
+                                                                </SelectContent>
+                                                            </Select>
+                                                        </div>
+                                                        <div className="grid grid-cols-4 items-center gap-4">
+                                                            <Label htmlFor="image" className="text-right">
+                                                                Role
+                                                            </Label>
+                                                            <Select>
+                                                                <SelectTrigger className="w-[180px]">
+                                                                    <SelectValue placeholder="Selecione" />
+                                                                </SelectTrigger>
+                                                                <SelectContent>
+                                                                    <SelectGroup>
+                                                                        <SelectLabel>Cargos</SelectLabel>
+                                                                        <SelectItem value="root">Root</SelectItem>
+                                                                        <SelectItem value="admin">Administrador</SelectItem>
+                                                                        <SelectItem value="journalist">Jornalista</SelectItem>
+                                                                    </SelectGroup>
+                                                                </SelectContent>
+                                                            </Select>
+                                                        </div>
+                                                    </div>
+                                                    <DialogFooter>
+                                                        <Button type="submit">Salvar</Button>
+                                                    </DialogFooter>
+                                                </DialogContent>
+                                            </Dialog>
                                         </TableCell>
                                     </TableRow>
                                 </TableBody>
--- src/components/dashboard/settings/settingsSidebar/index.tsx
+++ src/components/dashboard/settings/settingsSidebar/index.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { useRouter } from 'next/navigation';
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem } from "@/components/ui/select"; // Ajuste o import conforme necessário
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -7,8 +9,14 @@ interface SidebarProps {
 }
 
 export default function SettingsSidebar({ children }: SidebarProps) {
-
   const pathname = usePathname();
+  const router = useRouter();
+
+  const handleSelectChange = (value: string) => {
+    if (value) {
+      router.push(value);
+    }
+  };
 
   return (
     <main className="">
@@ -16,7 +24,7 @@ export default function SettingsSidebar({ children }: SidebarProps) {
         <h1 className="text-3xl font-semibold pb-6">Configurações</h1>
       </div>
       <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-        <nav className="grid gap-4 text-sm text-muted-foreground">
+        <nav className="hidden md:grid gap-4 text-sm text-muted-foreground">
           <Link
             href="/dashboard/settings"
             className={`font-semibold ${pathname === '/dashboard/settings' ? 'text-primary' : ''}`}
@@ -24,13 +32,22 @@ export default function SettingsSidebar({ children }: SidebarProps) {
           <Link
             href="/dashboard/settings/security"
             className={`font-semibold ${pathname === '/dashboard/settings/security' ? 'text-primary' : ''}`}
-
           >Segurança</Link>
-          <Link href="#"></Link>
-          <Link href="#"></Link>
-          <Link href="#"></Link>
-          <Link href="#"></Link>
         </nav>
+        <div className="md:hidden">
+          <Select onValueChange={handleSelectChange}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Páginas" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Página</SelectLabel>
+                <SelectItem value="/dashboard/settings">Geral</SelectItem>
+                <SelectItem value="/dashboard/settings/security">Segurança</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
         <div className="grid gap-6">
           {children}
         </div>
