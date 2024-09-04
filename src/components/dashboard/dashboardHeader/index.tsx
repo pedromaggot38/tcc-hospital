8495c324a01b22a2596b8ea811056c75e3cd705c
Pedro Sanches <pedromaggot38@gmail.com>
2024-09-04 18:38:22 -0300

    removendo linhas desnecessarias

--- src/components/dashboard/dashboardHeader/index.tsx
+++ src/components/dashboard/dashboardHeader/index.tsx
@@ -92,7 +92,6 @@ export function DashboardHeader() {
                             className="flex items-center gap-2 text-lg font-semibold"
                         >
                             <Package2 className="h-6 w-6" />
-                            <span className="sr-only">Acme Inc</span>
                         </Link>
 
                         {menuItems.map((item) => (
