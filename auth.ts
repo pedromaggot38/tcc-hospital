104ad32a3022062b4a5a3b00ea5417b1a6388eba
Pedro Sanches <pedromaggot38@gmail.com>
2024-09-04 03:52:01 -0300

    session.user.role presente no projeto todo

--- auth.ts
+++ auth.ts
@@ -13,7 +13,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       }
 
       if (token.role && session.user) {
-        session.user.role = token.role;
+        session.user.role = token.role
       }
 
       return session;
